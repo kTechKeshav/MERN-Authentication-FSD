dff902cd65bdd5f38f248506828eceb11bb7c4ba
Keshav Awasthi <ktech.professional2213127@gmail.com>
2025-06-28 18:47:03 +0530

    Refactor cookie settings to uncomment sameSite attribute in authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,7 +30,7 @@ export const register = async (req, res)=>{
             res.cookie('token', token, {
                   httpOnly: true,
                   secure: process.env.NODE_ENV === 'production',
-                  // sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict',
+                  sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict',
                   maxAge: 7 * 24 * 60 * 60 * 1000
             })
 
@@ -73,7 +73,7 @@ export const login = async (req, res)=>{
             res.cookie('token', token, {
                   httpOnly: true,
                   secure: process.env.NODE_ENV === 'production',
-                  // sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict',
+                  sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict',
                   maxAge: 7 * 24 * 60 * 60 * 1000
             });
 
@@ -89,7 +89,7 @@ export const logout = async (req, res)=>{
             res.clearCookie('token', {
                   httpOnly: true,
                   secure: process.env.NODE_ENV === 'production',
-                  // sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict'
+                  sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict'
             })
 
             return res.json({success: true, message: "Logged out"})
